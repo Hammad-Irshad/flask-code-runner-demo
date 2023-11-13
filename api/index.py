b4dc4789dadfc6b71d1db9ed5831c7ce4a87b414
hammad irshad_home <103359437+Hammad-Irshad@users.noreply.github.com>
2023-11-13 13:37:33 +0500

    Update index.py 5.0

--- api/index.py
+++ api/index.py
@@ -1,15 +1,32 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+    
+#     #return 'Hello, World!'
+#     with open('link.txt', 'r', encoding='utf-8') as file:
+#         links = file.readlines()
+#     return links
+
+# @app.route('/about')
+# def about():
+#     return 'About'
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
-    #return 'Hello, World!'
     with open('link.txt', 'r', encoding='utf-8') as file:
         links = file.readlines()
-    return links
+    return render_template('display_links.html', links=links)
 
 @app.route('/about')
 def about():
     return 'About'
+
+
