53f64e5dcdfa06125939388b3a2dfd5b8f6d85fc
hammad irshad_home <103359437+Hammad-Irshad@users.noreply.github.com>
2023-11-13 13:28:05 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    #return 'Hello, World!'
+     with open('link.txt', 'r', encoding='utf-8') as file:
+        links = file.readlines()
+    return render_template('display_links.html', links=links)
 
 @app.route('/about')
 def about():
